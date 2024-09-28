08a6b154d2254bc4c2074073d4cc9656df6c7995
mcdani978 <mcdani978@morrisville.edu>
2024-09-27 21:36:14 -0400

    Input binding for player actions set.

--- Project Boost/Assets/Scripts/Movement.cs
+++ Project Boost/Assets/Scripts/Movement.cs
@@ -13,6 +13,29 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+      if  (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Pressed SPACE - Thrusting");
+        }
+
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Rotating Left");
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Rotating Right");
+        }
     }
 }
