5c695e9a7bcda4e9cd656e9bb8872493f7be627a
mcdani978 <mcdani978@morrisville.edu>
2024-09-28 12:00:20 -0400

    Player movement improved with refined controls

--- Project Boost/Assets/Scripts/Movement.cs
+++ Project Boost/Assets/Scripts/Movement.cs
@@ -1,43 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 public class Movement : MonoBehaviour
 {
     Rigidbody rb;
-    // Start is called before the first frame update
+    public float thrustForce = 10f; 
+    public float rotationSpeed = 100f; 
+    public float moveSpeed = 5f; 
+
     void Start()
     {
-       rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         ProcessThrust();
         ProcessRotation();
+        ProcessHorizontalMovement(); 
     }
 
     void ProcessThrust()
     {
-      if  (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
-            rb.AddRelativeForce(Vector3.up);
+            rb.AddRelativeForce(Vector3.up * thrustForce); 
         }
-
     }
 
     void ProcessRotation()
     {
+        float rotationThisFrame = rotationSpeed * Time.deltaTime; 
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame); 
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame); 
+        }
+    }
+
+    void ProcessHorizontalMovement() 
+    {
         if (Input.GetKey(KeyCode.A))
         {
-            Debug.Log("Rotating Left");
+            rb.AddRelativeForce(Vector3.left * moveSpeed * Time.deltaTime); 
         }
-
         else if (Input.GetKey(KeyCode.D))
         {
-            Debug.Log("Rotating Right");
+            rb.AddRelativeForce(Vector3.right * moveSpeed * Time.deltaTime); 
         }
     }
 }
+
